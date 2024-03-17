8510ecd8d3665646daca7103ca6afca573b9c2e4
Roman Hera <romahera6@gmail.com>
2024-03-17 21:33:49 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*
+Демонстрація робити цикла for.
+ */
+class ForTest {
+    public static void main(String[] args) {
+        int x;
+        for(x = 0; x<10; x++)
+            System.out.println("Значення x: " + x );
+    }
+}
