551d364ceac193e569017664799f8485405819d0
Roman Hera <romahera6@gmail.com>
2024-03-17 20:51:03 +0200

    Update and rename Example2.java.iml to Java2.java

---
+++ Java2.java
@@ -0,0 +1,21 @@
+/*
+Ще один короткий приклад.
+Назва цього файлу Example2.java
+ */
+class Example2 {
+    public static void main(String[] args) {
+        int num; // Об'ява змінної по ім'ю num
+        num = 100;
+        System.out.println("Значення num: " + num);
+        num = num * 2;
+        System.out.print("Значення num * 2: ");
+        System.out.println(num);
+    }
+}
+
+/*
+int num; Об'являє змінну по імені num.
+Так виглядає форма об'явленої змінної: тип ім'я-змінна.
+num = 100; // пристоює змінну num значення 100.
+
+ */
